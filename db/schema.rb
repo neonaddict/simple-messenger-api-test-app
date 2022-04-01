fadf00cd711b8dd37d9d91308b9c633011b5b289
Konstantin Kim <kimkonsts@gmail.com>
2022-04-01 13:47:42 +0300

    add ChatUser model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_01_103335) do
+ActiveRecord::Schema.define(version: 2022_04_01_104458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chat_users", force: :cascade do |t|
+    t.bigint "chat_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chat_id"], name: "index_chat_users_on_chat_id"
+    t.index ["user_id"], name: "index_chat_users_on_user_id"
+  end
 
   create_table "chats", force: :cascade do |t|
     t.string "title"
