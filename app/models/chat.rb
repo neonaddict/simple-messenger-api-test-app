5c3b0db538acefa7fdc875f2e96eb43fab3361fd
Konstantin Kim <kimkonsts@gmail.com>
2022-04-01 19:06:45 +0300

    add routes for Chats and Messages controllers, add references and scope for chat-messages

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,9 @@
 class Chat < ApplicationRecord
   has_many :chat_users, dependent: :destroy
   has_many :users, through: :chat_users
-  has_many :messages
+  has_many :messages, -> { order('chat_messages.created_at ASC NULLS LAST') }
+
+  scope :order_by_recent_messages, lambda {
+    includes(:messages).order('chat_messages.created_at DESC NULLS LAST')
+  }
 end
